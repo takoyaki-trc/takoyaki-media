f03cabb1cba58149c995f5a03e575be4efbd1ea1
takoyaki-trc <takoyaki.trc@gmail.com>
2026-03-10 02:08:31 +0900

    Update collabs.js

--- town/assets/collabs.js
+++ town/assets/collabs.js
@@ -1,20 +1,23 @@
 /* =========================================================
    collabs.js
-   ✅ コラボ定義を1か所に集約
-   ✅ farm / roten 共通で使う
+   コラボ定義をここに集約
 ========================================================= */
+
 window.TAKOYAKI_COLLABS = [
+
   {
     collabId: "col_gratan_2026",
     seedId: "seed_col_gratan_2026",
     name: "【コラボ】ぐらたんのタネ",
     img: "https://takoyaki-trc.github.io/takoyaki-media/town/assets/images/tane/col1.png",
-    hidden: false,
-    blocked: false,
 
     boothUrl: "https://takoyaki-toreka.booth.pm/item_lists/m7YToKe5",
+
     periodStart: "2026-03-20",
     periodEnd: "2026-04-19",
+
+    hidden: false,
+    blocked: false,
 
     grow1: "https://ul.h3z.jp/cq1soJdm.gif",
     grow2: "https://ul.h3z.jp/I6Iu4J32.gif",
@@ -25,12 +28,14 @@ window.TAKOYAKI_COLLABS = [
     seedId: "seed_col_ghost_2026",
     name: "【コラボ】GHOSTのタネ",
     img: "https://takoyaki-trc.github.io/takoyaki-media/town/assets/images/tane/col2.png",
-    hidden: false,
-    blocked: false,
 
     boothUrl: "https://takoyaki-toreka.booth.pm/item_lists/m7YToKe5",
+
     periodStart: "2026-04-20",
     periodEnd: "2026-05-19",
+
+    hidden: false,
+    blocked: false,
 
     grow1: "https://ul.h3z.jp/cq1soJdm.gif",
     grow2: "https://ul.h3z.jp/I6Iu4J32.gif",
@@ -41,15 +46,16 @@ window.TAKOYAKI_COLLABS = [
     seedId: "seed_col_hold_2026",
     name: "【コラボ】HOLDのタネ",
     img: "https://takoyaki-trc.github.io/takoyaki-media/town/assets/images/tane/hold.png",
-    hidden: true,
-    blocked: true,
 
     boothUrl: "",
     periodStart: "",
     periodEnd: "",
 
-    grow1: "https://ul.h3z.jp/cq1soJdm.gif",
-    grow2: "https://ul.h3z.jp/I6Iu4J32.gif",
+    hidden: true,
+    blocked: true,
+
+    grow1: "",
+    grow2: "",
   },
 
   {
@@ -57,14 +63,17 @@ window.TAKOYAKI_COLLABS = [
     seedId: "seed_ann_2026",
     name: "ハーフアニバーサリーのタネ",
     img: "https://takoyaki-trc.github.io/takoyaki-media/town/assets/images/tane/anv1.png",
+
+    boothUrl: "https://takoyaki-toreka.booth.pm/item_lists/m7YToKe5",
+
+    periodStart: "2026-03-10",
+    periodEnd: "2026-05-31",
+
     hidden: false,
     blocked: false,
 
-    boothUrl: "https://takoyaki-toreka.booth.pm/item_lists/m7YToKe5",
-    periodStart: "2026-03-12",
-    periodEnd: "2026-05-19",
-
     grow1: "https://takoyaki-card.com/town/assets/images/anniversary/tane1.gif",
     grow2: "https://takoyaki-card.com/town/assets/images/anniversary/tane2.gif",
-  },
+  }
+
 ];
