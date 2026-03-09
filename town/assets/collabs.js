6908c64885df2406f355fb276a8d745a6b9273fa
takoyaki-trc <takoyaki.trc@gmail.com>
2026-03-10 01:45:41 +0900

    Create collabs.js

---
+++ town/assets/collabs.js
@@ -0,0 +1,70 @@
+/* =========================================================
+   collabs.js
+   ✅ コラボ定義を1か所に集約
+   ✅ farm / roten 共通で使う
+========================================================= */
+window.TAKOYAKI_COLLABS = [
+  {
+    collabId: "col_gratan_2026",
+    seedId: "seed_col_gratan_2026",
+    name: "【コラボ】ぐらたんのタネ",
+    img: "https://takoyaki-trc.github.io/takoyaki-media/town/assets/images/tane/col1.png",
+    hidden: false,
+    blocked: false,
+
+    boothUrl: "https://takoyaki-toreka.booth.pm/item_lists/m7YToKe5",
+    periodStart: "2026-03-20",
+    periodEnd: "2026-04-19",
+
+    grow1: "https://ul.h3z.jp/cq1soJdm.gif",
+    grow2: "https://ul.h3z.jp/I6Iu4J32.gif",
+  },
+
+  {
+    collabId: "col_ghost_2026",
+    seedId: "seed_col_ghost_2026",
+    name: "【コラボ】GHOSTのタネ",
+    img: "https://takoyaki-trc.github.io/takoyaki-media/town/assets/images/tane/col2.png",
+    hidden: false,
+    blocked: false,
+
+    boothUrl: "https://takoyaki-toreka.booth.pm/item_lists/m7YToKe5",
+    periodStart: "2026-04-20",
+    periodEnd: "2026-05-19",
+
+    grow1: "https://ul.h3z.jp/cq1soJdm.gif",
+    grow2: "https://ul.h3z.jp/I6Iu4J32.gif",
+  },
+
+  {
+    collabId: "col_hold_2026",
+    seedId: "seed_col_hold_2026",
+    name: "【コラボ】HOLDのタネ",
+    img: "https://takoyaki-trc.github.io/takoyaki-media/town/assets/images/tane/hold.png",
+    hidden: true,
+    blocked: true,
+
+    boothUrl: "",
+    periodStart: "",
+    periodEnd: "",
+
+    grow1: "https://ul.h3z.jp/cq1soJdm.gif",
+    grow2: "https://ul.h3z.jp/I6Iu4J32.gif",
+  },
+
+  {
+    collabId: "ann_2026",
+    seedId: "seed_ann_2026",
+    name: "ハーフアニバーサリーのタネ",
+    img: "https://takoyaki-trc.github.io/takoyaki-media/town/assets/images/tane/anv1.png",
+    hidden: false,
+    blocked: false,
+
+    boothUrl: "https://takoyaki-toreka.booth.pm/item_lists/m7YToKe5",
+    periodStart: "2026-03-12",
+    periodEnd: "2026-05-19",
+
+    grow1: "https://takoyaki-card.com/town/assets/images/anniversary/tane1.gif",
+    grow2: "https://takoyaki-card.com/town/assets/images/anniversary/tane2.gif",
+  },
+];
