5283e067bb9a8a9a5dda9f2fe4f6fb1713de081f
takoyaki-trc <takoyaki.trc@gmail.com>
2026-03-13 04:31:35 +0900

    Update takofarm.js

--- town/assets/takofarm.js
+++ town/assets/takofarm.js
@@ -248,7 +248,7 @@
     { id: "TP-004", name: "クリスマスたこぴ", img: "https://ul.h3z.jp/FGEKvxhK.png", rarity: "N" },
     { id: "TP-005", name: "お年玉たこぴ", img: "https://ul.h3z.jp/OPz58Wt6.png", rarity: "N" },
     { id: "TP-006", name: "バレンタインたこぴ", img: "https://ul.h3z.jp/J0kj3CLb.png", rarity: "N" },
-    { id: "TP-007", name: "花見たこぴ", img: "https://ul.h3z.jp/KrCy4WQb.png", rarity: "UR" },
+    { id: "TP-007", name: "花見たこぴ", img: "https://ul.h3z.jp/KrCy4WQb.png", rarity: "N" },
     { id: "TP-008", name: "入学たこぴ", img: "https://ul.h3z.jp/DidPdK9b.png", rarity: "UR" },
   ];
 
