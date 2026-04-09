b3a067442122c8fff8dcd8ccb710edf4795c38ba
takoyaki-trc <takoyaki.trc@gmail.com>
2026-04-09 22:33:01 +0900

    takomin/takomin-cards.js の作成

---
+++ takomin/takomin-cards.js
@@ -0,0 +1,502 @@
+window.TAKOMIN_CARDS = [
+  {
+    id: "TN-001",
+    name: "黒き真珠イカさま焼き",
+    rarity: "UR",
+    atk: 95,
+    def: 88,
+    taste: 39,
+    love: 110,
+    img: "assets/images/1stcard/001ur1.png"
+  },
+  {
+    id: "TN-002",
+    name: "熱々地獄の給たこ所",
+    rarity: "R",
+    atk: 25,
+    def: 11,
+    taste: 14,
+    love: 16,
+    img: "https://ul.h3z.jp/otr0dAQi.jpg"
+  },
+  {
+    id: "TN-003",
+    name: "爆走！たこ焼きライダー菜々",
+    rarity: "R",
+    atk: 22,
+    def: 10,
+    taste: 13,
+    love: 20,
+    img: "https://ul.h3z.jp/06HrUPMT.jpg"
+  },
+  {
+    id: "TN-004",
+    name: "見えるフリ焼き",
+    rarity: "SR",
+    atk: 50,
+    def: 70,
+    taste: 60,
+    love: 100,
+    img: "https://ul.h3z.jp/irs6Sxoy.jpg"
+  },
+  {
+    id: "TN-005",
+    name: "たこ焼きタワー112",
+    rarity: "N",
+    atk: 1,
+    def: 14,
+    taste: 18,
+    love: 8,
+    img: "https://ul.h3z.jp/LoXMSiYd.jpg"
+  },
+  {
+    id: "TN-006",
+    name: "塩顔パレード焼き",
+    rarity: "N",
+    atk: 9,
+    def: 1,
+    taste: 23,
+    love: 6,
+    img: "https://ul.h3z.jp/7L7rcrnM.jpg"
+  },
+  {
+    id: "TN-007",
+    name: "ローソク出せ！",
+    rarity: "UR",
+    atk: 10,
+    def: 80,
+    taste: 8,
+    love: 102,
+    img: "assets/images/1stcard/007ur1.png"
+  },
+  {
+    id: "TN-008",
+    name: "明太ギャラクシー焼き",
+    rarity: "R",
+    atk: 6,
+    def: 12,
+    taste: 32,
+    love: 15,
+    img: "https://ul.h3z.jp/xye1uAfV.jpg"
+  },
+  {
+    id: "TN-009",
+    name: "塩マヨ露天焼き",
+    rarity: "N",
+    atk: 8,
+    def: 31,
+    taste: 4,
+    love: 6,
+    img: "https://ul.h3z.jp/bF9QmTE8.jpg"
+  },
+  {
+    id: "TN-010",
+    name: "焼く者ノ証",
+    rarity: "SR",
+    atk: 95,
+    def: 45,
+    taste: 100,
+    love: 4,
+    img: "https://ul.h3z.jp/6A2LOn4A.jpg"
+  },
+  {
+    id: "TN-011",
+    name: "チーズ火山焼き",
+    rarity: "N",
+    atk: 4,
+    def: 7,
+    taste: 31,
+    love: 1,
+    img: "https://ul.h3z.jp/BEj3BIcP.jpg"
+  },
+  {
+    id: "TN-012",
+    name: "揚げ玉会議焼き",
+    rarity: "N",
+    atk: 8,
+    def: 10,
+    taste: 12,
+    love: 4,
+    img: "https://ul.h3z.jp/vVw2FjQp.jpg"
+  },
+  {
+    id: "TN-013",
+    name: "くたびれ塩こしょう焼き",
+    rarity: "N",
+    atk: 21,
+    def: 5,
+    taste: 18,
+    love: 6,
+    img: "https://ul.h3z.jp/DlX5pLJ5.jpg"
+  },
+  {
+    id: "TN-014",
+    name: "世界たこ焼き釣り選手権大会",
+    rarity: "R",
+    atk: 11,
+    def: 10,
+    taste: 14,
+    love: 26,
+    img: "https://ul.h3z.jp/cyekwiam.jpg"
+  },
+  {
+    id: "TN-015",
+    name: "顔コイン",
+    rarity: "SR",
+    atk: 65,
+    def: 70,
+    taste: 40,
+    love: 105,
+    img: "https://ul.h3z.jp/7GUyGDU1.jpg"
+  },
+  {
+    id: "TN-016",
+    name: "たこ焼き、発射オーライ",
+    rarity: "N",
+    atk: 11,
+    def: 7,
+    taste: 17,
+    love: 11,
+    img: "https://ul.h3z.jp/50WYMkYw.jpg"
+  },
+  {
+    id: "TN-017",
+    name: "たこ焼きマニフェスト",
+    rarity: "R",
+    atk: 18,
+    def: 18,
+    taste: 16,
+    love: 10,
+    img: "https://ul.h3z.jp/zeSwFyjz.jpg"
+  },
+  {
+    id: "TN-018",
+    name: "ゆのかわの主",
+    rarity: "N",
+    atk: 6,
+    def: 18,
+    taste: 16,
+    love: 10,
+    img: "https://ul.h3z.jp/mkLBMxIT.jpg"
+  },
+  {
+    id: "TN-019",
+    name: "誤入店トラップ",
+    rarity: "N",
+    atk: 2,
+    def: 22,
+    taste: 14,
+    love: 10,
+    img: "https://ul.h3z.jp/YfON5rBJ.jpg"
+  },
+  {
+    id: "TN-020",
+    name: "ピック不要の真実",
+    rarity: "SR",
+    atk: 62,
+    def: 58,
+    taste: 100,
+    love: 52,
+    img: "https://ul.h3z.jp/Bu1pk4ul.jpg"
+  },
+  {
+    id: "TN-021",
+    name: "たこ焼き、流れて候",
+    rarity: "N",
+    atk: 14,
+    def: 9,
+    taste: 6,
+    love: 20,
+    img: "https://ul.h3z.jp/O4s1VpWd.jpg"
+  },
+  {
+    id: "TN-022",
+    name: "たこ焼きダーツインフェルノ《對馬裕佳子プロ🎯》",
+    rarity: "R",
+    atk: 19,
+    def: 8,
+    taste: 14,
+    love: 22,
+    img: "https://ul.h3z.jp/Prf7KxRk.jpg"
+  },
+  {
+    id: "TN-023",
+    name: "芝生かたこ焼きか大会",
+    rarity: "N",
+    atk: 10,
+    def: 10,
+    taste: 8,
+    love: 19,
+    img: "https://ul.h3z.jp/FZcOaXY8.jpg"
+  },
+  {
+    id: "TN-024",
+    name: "温泉女神のありがた迷惑",
+    rarity: "N",
+    atk: 21,
+    def: 9,
+    taste: 2,
+    love: 14,
+    img: "https://ul.h3z.jp/A6WhBsqj.jpg"
+  },
+  {
+    id: "TN-025",
+    name: "たこ焼き化石in函館山",
+    rarity: "LR",
+    atk: 105,
+    def: 92,
+    taste: 106,
+    love: 137,
+    img: "https://ul.h3z.jp/NEuFQ7PB.png"
+  },
+  {
+    id: "TN-026",
+    name: "たこ焼き48回リボ払い",
+    rarity: "N",
+    atk: 5,
+    def: 8,
+    taste: 22,
+    love: 10,
+    img: "https://ul.h3z.jp/hz7JXyky.jpg"
+  },
+  {
+    id: "TN-027",
+    name: "全身たこ焼きダイエット",
+    rarity: "N",
+    atk: 19,
+    def: 7,
+    taste: 6,
+    love: 18,
+    img: "https://ul.h3z.jp/FQ3poZLg.jpg"
+  },
+  {
+    id: "TN-028",
+    name: "自己啓発たこ塾《井上諒プロ🎯》",
+    rarity: "N",
+    atk: 99,
+    def: 4,
+    taste: 9,
+    love: 0,
+    img: "https://ul.h3z.jp/sPChFFlG.jpg"
+  },
+  {
+    id: "TN-029",
+    name: "カロリーゼロ理論《仁木治プロ🎯》",
+    rarity: "N",
+    atk: 0,
+    def: 99,
+    taste: 0,
+    love: 0,
+    img: "https://ul.h3z.jp/4HEbt3YP.jpg"
+  },
+  {
+    id: "TN-030",
+    name: "ガチャたこ焼き",
+    rarity: "SR",
+    atk: 100,
+    def: 50,
+    taste: 110,
+    love: 20,
+    img: "https://ul.h3z.jp/kFpjcqSv.jpg"
+  },
+  {
+    id: "TN-031",
+    name: "行列の最後尾が別県",
+    rarity: "N",
+    atk: 40,
+    def: 60,
+    taste: 70,
+    love: 30,
+    img: "https://ul.h3z.jp/LBdFqlLI.jpg"
+  },
+  {
+    id: "TN-032",
+    name: "国境超えた恋",
+    rarity: "R",
+    atk: 70,
+    def: 40,
+    taste: 90,
+    love: 40,
+    img: "https://ul.h3z.jp/9AZcVNmR.jpg"
+  },
+  {
+    id: "TN-033",
+    name: "鉄板のビーナス",
+    rarity: "UR",
+    atk: 90,
+    def: 40,
+    taste: 140,
+    love: 110,
+    img: "assets/images/1stcard/033ur1.png"
+  },
+  {
+    id: "TN-034",
+    name: "エシカル過剰焼き",
+    rarity: "N",
+    atk: 85,
+    def: 5,
+    taste: 20,
+    love: 90,
+    img: "https://ul.h3z.jp/KRkSq4WD.jpg"
+  },
+  {
+    id: "TN-035",
+    name: "デリバリー長距離便",
+    rarity: "R",
+    atk: 70,
+    def: 45,
+    taste: 65,
+    love: 60,
+    img: "https://ul.h3z.jp/z0xhODVy.jpg"
+  },
+  {
+    id: "TN-036",
+    name: "マヨネーズ詐欺",
+    rarity: "N",
+    atk: 65,
+    def: 25,
+    taste: 68,
+    love: 52,
+    img: "https://ul.h3z.jp/NzVgPYdG.jpg"
+  },
+  {
+    id: "TN-037",
+    name: "勘違いデート",
+    rarity: "N",
+    atk: 78,
+    def: 18,
+    taste: 69,
+    love: 47,
+    img: "https://ul.h3z.jp/riYYAnEi.jpg"
+  },
+  {
+    id: "TN-038",
+    name: "恋落ちマッチング",
+    rarity: "R",
+    atk: 82,
+    def: 20,
+    taste: 78,
+    love: 68,
+    img: "https://ul.h3z.jp/BPEoWjuY.jpg"
+  },
+  {
+    id: "TN-039",
+    name: "ドローン誤配達",
+    rarity: "SR",
+    atk: 100,
+    def: 55,
+    taste: 110,
+    love: 15,
+    img: "https://ul.h3z.jp/70A10oHf.jpg"
+  },
+  {
+    id: "TN-040",
+    name: "推し活たこ団扇",
+    rarity: "SR",
+    atk: 105,
+    def: 50,
+    taste: 110,
+    love: 15,
+    img: "https://ul.h3z.jp/jY5MVsrt.jpg"
+  },
+  {
+    id: "TN-041",
+    name: "玉の上にも三年",
+    rarity: "N",
+    atk: 65,
+    def: 25,
+    taste: 8,
+    love: 30,
+    img: "https://ul.h3z.jp/pQg0jZMy.jpg"
+  },
+  {
+    id: "TN-042",
+    name: "たこ焼きループザループ",
+    rarity: "R",
+    atk: 65,
+    def: 30,
+    taste: 95,
+    love: 50,
+    img: "https://ul.h3z.jp/vxKamb6f.jpg"
+  },
+  {
+    id: "TN-043",
+    name: "転生したら即売れたこ焼き",
+    rarity: "N",
+    atk: 65,
+    def: 45,
+    taste: 68,
+    love: 22,
+    img: "https://ul.h3z.jp/I3JWnpoL.jpg"
+  },
+  {
+    id: "TN-044",
+    name: "白い契約(稲石裕プロ🎯)",
+    rarity: "R",
+    atk: 99,
+    def: 0,
+    taste: 0,
+    love: Infinity,
+    img: "https://ul.h3z.jp/bC1B4WkQ.jpg"
+  },
+  {
+    id: "TN-045",
+    name: "ドリームファイト",
+    rarity: "UR",
+    atk: 85,
+    def: 90,
+    taste: 140,
+    love: 65,
+    img: "assets/images/1stcard/045ur1.png"
+  },
+  {
+    id: "TN-046",
+    name: "ごますりたこ焼き",
+    rarity: "N",
+    atk: 65,
+    def: 28,
+    taste: 75,
+    love: 32,
+    img: "https://ul.h3z.jp/tuLsTiaz.jpg"
+  },
+  {
+    id: "TN-047",
+    name: "ボスゲート",
+    rarity: "R",
+    atk: 77,
+    def: 34,
+    taste: 71,
+    love: 48,
+    img: "https://ul.h3z.jp/GHWrtaYk.jpg"
+  },
+  {
+    id: "TN-048",
+    name: "店主反撃レビュー《佐俣雄一郎🎯》",
+    rarity: "N",
+    atk: 50,
+    def: 50,
+    taste: 50,
+    love: Infinity,
+    img: "https://ul.h3z.jp/ge8b4cQ5.jpg"
+  },
+  {
+    id: "TN-049",
+    name: "たこ焼きの御神体",
+    rarity: "SR",
+    atk: 95,
+    def: 45,
+    taste: 100,
+    love: 4,
+    img: "https://ul.h3z.jp/GQ8H0lGq.jpg"
+  },
+  {
+    id: "TN-050",
+    name: "焼かれし記憶、ソースに還る",
+    rarity: "LR",
+    atk: 110,
+    def: 95,
+    taste: 98,
+    love: 137,
+    img: "assets/images/1stcard/050lr1.png"
+  }
+];
